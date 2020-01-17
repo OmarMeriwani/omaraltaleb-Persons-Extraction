50c9a4e588b95114c6bfe1737aac320a0f60f4ec
Omar Meriwani <omar.sirwan@gmail.com>
2020-01-17 13:56:56 +0000

    Adding classification dataset

--- createClassificationDataset.py
+++ createClassificationDataset.py
@@ -1,7 +1,8 @@
-
+import pandas as pd
 
 df = pd.read_csv('OmarAlTalebSheet.csv',encoding='utf-8',header=0)
 print(df.head())
+NamesAndContentList = []
 for i in range(0,len(df.values)):
     personName = str(df.loc[i].values[0])
     personName = personName.replace('  ', ' ')
@@ -11,3 +12,6 @@ for i in range(0,len(df.values)):
     content = str(df.loc[i].values[4])
     sents = df.loc[i].values[5]
     events = df.loc[i].values[6]
+    NamesAndContentList.append([personName, content])
+df2 = pd.DataFrame(NamesAndContentList,columns=['name','content'])
+df2.to_csv('classificationDS.csv',encoding='utf-8')
